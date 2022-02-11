bdffec56e673d6942a8d8492104675c44e60e9b9
Tran Ngoc Phuoc <phuoc04012000@gmail.com>
2022-02-11 10:20:45 +0700

    Navigating programmatically

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const Home = () => {
+    const router = useRouter()
+
+    const handleClick = () => {
+        console.log('Placing your order')
+        router.push('/product')
+    }
+
     return (<>
         <h1>Home Page</h1>
         <Link href='/blog'>
@@ -9,6 +17,9 @@ const Home = () => {
         <Link href='/product'>
             <a>Products</a>
         </Link>
+        <button onClick={handleClick}>
+            Place Order
+        </button>
     </>)
 }
 
