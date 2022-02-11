c65d55fb42a304d0cd601be0d4c62b4aca4017cf
Tran Ngoc Phuoc <phuoc04012000@gmail.com>
2022-02-11 09:55:04 +0700

    Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import Link from 'next/link'
+
 const Home = () => {
-    return <h1>Home Page</h1>
+    return (<>
+        <h1>Home Page</h1>
+        <Link href='/blog'>
+            <a>Blog</a>
+        </Link>
+        <Link href='/product'>
+            <a>Products</a>
+        </Link>
+    </>)
 }
 
 export default Home
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,11 +1,23 @@
-const Product = () => {
+import Link from 'next/link'
+
+const ProductList = () => {
+    const pids = [1, 2, 3]
+
     return (
         <>
-            <h1>Product 1</h1>
-            <h1>Product 2</h1>
-            <h1>Product 3</h1>
+            <Link href='/'>
+                <a>Home</a>
+            </Link>
+            {/* {products[1]} */}
+            {
+                pids.map((pid) => (
+                    <Link key={pid} href="/product/[productId]" as={`/product/${pid}`}>
+                        <a>Product {pid}</a>
+                    </Link>
+                ))
+            }
         </>
     )
 }
 
-export default Product
+export default ProductList
